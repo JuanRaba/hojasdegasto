acb5cce5a8ced8d514bd56ae16257fd46726ef5b
juanraba <jrabanillog@gmail.com>
2018-11-15 13:32:17 -0300

    add relations

--- app/models/expenses_sheet.rb
+++ app/models/expenses_sheet.rb
@@ -1,4 +1,5 @@
 class ExpensesSheet < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: :user_id
   has_many :expenses
+  has_many :users, through: :asociations
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :expenses
+  has_many :expensesSheets, through: :asociations
 end
