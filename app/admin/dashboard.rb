a458ca11de97dcda26eb98a00cd06e382272cf3e
juanraba <jrabanillog@gmail.com>
2019-01-15 18:56:40 -0300

    active_admin Dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,31 +3,38 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    # to add more watch https://activeadmin.info/12-arbre-components.html
+    columns do
+      column do
+        panel "Recent 5Expenses" do
+          ul do
+            Expense.last(5).map do |expense|
+              li expense.show_name
+            end
+          end
+        end
       end
+
+      column do
+        panel "Recent 5Users" do
+          ul do
+            User.last(5).map do |u|
+              li link_to(u.show_name, admin_user_path(u))
+            end
+          end
+        end
+      end
+
+      column do
+        panel "Expense count" do
+          ul do
+            li "Expenses creados: #{Expense.count}"
+            li "ExpensesSheet creados: #{ExpensesSheet.count}"
+          end
+        end
+      end
+
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
   end # content
 end
